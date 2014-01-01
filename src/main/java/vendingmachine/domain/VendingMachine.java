3583b18088b54033f656f39f901b2356c0ad8250
bartosz.walacik <bartosz.walacik@allegro.pl>
2014-01-01 21:59:19 +0100

    better domain model,  coins (nickels, dimes, and quarters, pennies)
    added initial MachineState.READY and initial display message

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,4 +1,19 @@
 package vendingmachine.domain;
 
+import static vendingmachine.domain.MachineState.READY;
+
 public class VendingMachine {
+    private MachineState state;
+
+    public VendingMachine() {
+        state = READY;
+    }
+
+    public String getDisplay() {
+        if (state == READY) {
+            return "INSERT A COIN";
+        }
+
+        return "";
+    }
 }
